fe98ebed47b99ed649d2167e441dd7a5764194eb
Farhan Nazeer <farhan-n4@hotmail.com>
2021-06-22 20:44:04 -0400

    Second commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Initial Commit!</Text>
+      <Text>Second Commit!</Text>
       <StatusBar style="auto" />
     </View>
   );
