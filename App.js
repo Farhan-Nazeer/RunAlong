43e388c5f23fd6c981d08aac16df7e281bb755fd
Ramonito Copreros Jr <ramonito.copreros.jr@appliedbrainresearch.com>
2021-06-24 18:05:37 -0400

    Test ramonito commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Second Commit!</Text>
+      <Text>Second Commit!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
