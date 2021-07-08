7ebef3621d7f8f520b4ff5797120827ad8d43527
Farhan Nazeer <farhan-n4@hotmail.com>
2021-07-08 19:44:59 -0400

    Added Expo Google Map API to app with starting location

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import React, { useState, useEffect } from "react";
 import { Platform, Text, View, StyleSheet } from "react-native";
 import * as Location from "expo-location";
 
-//remember to install whatever npm stuff "exp install expo-location"
-
+import MapOnScreen from "./components/Run";
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  //TODO: figure out actual intial states for lat long
+  const [latitudeValue, setLatitudeValue] = useState(4);
+  const [longitudeValue, setLongitudeValue] = useState(4);
 
   useEffect(() => {
     (async () => {
@@ -20,6 +22,8 @@ export default function App() {
         accuracy: Location.Accuracy.High,
       });
       setLocation(location);
+      setLatitudeValue(location.coords.latitude)
+      setLongitudeValue(location.coords.longitude)
     })();
   }, []);
 
@@ -29,9 +33,11 @@ export default function App() {
   } else if (location) {
     text = JSON.stringify(location);
   }
+
   return (
     <View style={styles.container}>
-      <Text>{text}</Text>
+      <MapOnScreen lat={latitudeValue} long={longitudeValue}/>
+      <Text>{text}</Text> 
       <StatusBar style="auto" />
     </View>
   );
