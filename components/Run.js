dd377cf4b2e64302bf68ce06e78f91c7211605fb
Farhan Nazeer <farhan-n4@hotmail.com>
2021-07-13 20:33:41 -0400

    Added marker and automatic map centering

--- components/Run.js
+++ components/Run.js
@@ -1,24 +1,80 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+//import * as React from "react";
+import MapView, { Marker } from "react-native-maps";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
-export default function MapOnScreen(props)  {
-    return (
-        <View style={styles.container}>
-        <MapView style={styles.map} initialRegion={{latitude: props.lat, longitude: props.long, latitudeDelta: 0.0922, longitudeDelta: 0.0421}} />
-        </View>
+export default function MapOnScreen(props) {
+  const mapRef = React.createRef();
+  //TODO: look into animateCamera
+  onNewPosition = () => {
+    console.log("called");
+    //TODO: Replace hard coded values with actual variable
+    mapRef.current.animateToRegion(
+      {
+        latitude: 37.4219983,
+        longitude: -122.084,
+        latitudeDelta: 0.04,
+        longitudeDelta: 0.04,
+      },
+      1000
     );
+  };
+
+  useEffect(() => {
+    (async () => {
+      console.log("here");
+      const options = {
+        enableHighAccuracy: true,
+        timeInterval: 1000,
+        distanceInterval: 1,
+      };
+      watcher = await Location.watchPositionAsync(
+        options,
+        onNewPosition,
+        mapRef
+      );
+    })();
+  }, []);
+
+  function componentWillUnmount() {
+    watcher.remove();
+  }
+  console.log(props.lat, props.long);
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        initialRegion={{
+          latitude: props.lat,
+          longitude: props.long,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        provider="google"
+      >
+        <Marker
+          coordinate={{ latitude: props.lat, longitude: props.long }}
+          pinColor={"red"}
+          title={"title"}
+          description={"description"}
+        />
+      </MapView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
+  container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-},
-map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-},
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
 });
